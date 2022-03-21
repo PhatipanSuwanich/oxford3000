f980824c570ab6ebb2ac64c8d3f0c7a6e881c4f4
benz466666@gmail.com <BenzDroid39>
2022-03-21 12:12:57 +0700

    first commit

--- lib/screens/learn.dart
+++ lib/screens/learn.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: empty_constructor_bodies, prefer_const_constructors, non_constant_identifier_names
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
@@ -21,55 +23,60 @@ class _LearnPageState extends State<LearnPage> {
   void initState() {
     super.initState();
     vocab = Vocabulary.fromJson(mockJsonMenu);
+    var num = Random().nextInt(50);
+    vocab.eng!.shuffle(Random(num));
+    vocab.th!.shuffle(Random(num));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        height: MediaQuery.of(context).size.height * 0.35,
-        padding: EdgeInsets.all(30),
-        constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/bg.jpg"), fit: BoxFit.cover),
-        ),
-        child: Card(
-          // child: Column(
-          //   mainAxisAlignment: MainAxisAlignment.center,
-          //   crossAxisAlignment: CrossAxisAlignment.center,
-          //   children: [
-          //     Text(vocab.th![2],
-          //         style: TextStyle(
-          //             backgroundColor: Colors.white,
-          //             fontSize: 32.0,
-          //             fontWeight: FontWeight.bold)),
-          //     Container(
-          //       color: Colors.white,
-          //       child: TextButton.icon(
-          //           onPressed: () => speakWord(),
-          //           icon: Icon(
-          //             Icons.volume_up_sharp,
-          //             color: Colors.teal,
-          //           ),
-          //           label: Text(
-          //             'English Sound',
-          //             style: TextStyle(
-          //               fontSize: 18,
-          //               color: Colors.teal,
-          //             ),
-          //           )),
-          //     ),
-          //     SizedBox(height: 40),
-          //     Answer(),
-          //     SizedBox(height: 50),
-          //     CheckButton(),
-          //   ],
-          // ),
+    return Container(
+      padding: EdgeInsets.all(30),
+      // constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/bg.jpg"), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        resizeToAvoidBottomInset: false,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(vocab.th![2],
+                style: TextStyle(
+                    backgroundColor: Colors.white,
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold)),
+            englishSound(),
+            SizedBox(height: 40),
+            Answer(),
+            SizedBox(height: 50),
+            CheckButton(),
+          ],
         ),
       ),
     );
+  }
+
+  Container englishSound() {
+    return Container(
+            color: Colors.white,
+            child: TextButton.icon(
+                onPressed: () => speakWord(),
+                icon: Icon(
+                  Icons.volume_up_sharp,
+                  color: Colors.teal,
+                ),
+                label: Text(
+                  'English Sound',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.teal,
+                  ),
+                )),
+          );
   }
 
   SizedBox CheckButton() {
